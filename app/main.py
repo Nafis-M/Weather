59be43f8014d16dbf52c87f4cf2dd3e079366142
Nafis-M <122175480+Nafis-M@users.noreply.github.com>
2025-02-23 20:45:13 -0500

    Testing the API Key
    Testing to see if we can get Open Weather to work with our Fast API system
    
    This program will now display the current weather, humidity, and temperature in the city of Detroit, MI

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
-
+import requests
 app = FastAPI()
+
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/test")
+def testrun():
+    url = "https://api.openweathermap.org/data/2.5/weather?lat=42.3314&lon=83.0458&appid={API_KEY}"
+    trial = requests.get(url).json()
+    #getting the current weather description
+    current = trial["weather"]
+    weather = current[0]["description"]
+    #creating function to get the current temperature
+    temperature = trial["main"]
+    temp = temperature["temp"]
+    feelings = temperature["feels_like"]
+    humidity = temperature["humidity"]
+    
+    return {'current weather is: ': weather, 'temperature is: ': temp, 'feelings is: ': feelings, 'Humidity is: ': humidity}
 
